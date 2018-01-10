c55c3fef4614b272e1c151a7156e6a25408e87e9
Gadzhi Kharkharov <gadzhi.kha@gmail.com>
2018-01-10 22:01:07 +0100

    add basic maze generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+	public Maze mazePrefab;
+	private Maze mazeInstance;
 
-    // Use this for initialization
     void Start()
     {
-
+        BeginGame();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
 
+    void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
     }
 }
